2834f15650ae84aae926d4ca7ac91113853f48c6
aKi-1201 <b10501015@ntu.edu.tw>
2025-12-19 09:36:42 +0800

    Initial Commit

---
+++ Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final
+//
+//  Created by 114-1iosClassStudent05 on 2025/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by 114-1iosClassStudent05 on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
